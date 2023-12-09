cd76ecd4ca7ef5a3c0e0734df0e3633ae1b97d6b
sandovalmelo <juniormelo777@gmail.com>
2023-12-09 19:04:55 -0300

    add base html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import "./App.css";
-
 function App() {
 	return (
 		<div className="App">
-			<h1>App</h1>
+			<h1>FAQs</h1>
+
+			<h2>What is Frontend Mentor, and how will it help me?</h2>
+			<p>
+				Frontend Mentor offers realistic coding challenges to help developers improve their frontend
+				coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and
+				ideal for portfolio building.
+			</p>
+
+			<h2>Is Frontend Mentor free?</h2>
+			<p>
+				Yes, Frontend Mentor offers both free and premium coding challenges, with the free option
+				providing access to a range of projects suitable for all skill levels.
+			</p>
+
+			<h2>Can I use Frontend Mentor projects in my portfolio?</h2>
+			<p>
+				Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+				way to showcase your skills to potential employers!
+			</p>
+
+			<h2>How can I get help if I'm stuck on a Frontend Mentor challenge?</h2>
+			<p>
+				The best place to get help is inside Frontend Mentor's Discord community. There's a help
+				channel where you can ask questions and seek support from other community members.
+			</p>
 		</div>
 	);
 }
