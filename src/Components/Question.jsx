244e4bd8123ab25abc37312a601dd704008991b6
sandovalmelo <juniormelo777@gmail.com>
2023-12-09 20:41:55 -0300

    add toggle question

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./Question.scss";
 
-function Question({ question, answer }) {
+function Question({ question, answer, id, open, setOpen }) {
+	function handleClick() {
+		setOpen(id === open ? "null" : id);
+	}
+
 	return (
 		<div className="question">
-			<h2 className="question-title">{question}</h2>
-			<p className="question-answer">{answer}</p>
+			<div className="question-box" onClick={handleClick}>
+				<h2 className="question-title">{question}</h2>
+				<span>
+					<img
+						src={id === open ? "/images/icon-minus.svg" : "/images/icon-plus.svg"}
+						alt="Toggle Icon"
+					/>
+				</span>
+			</div>
+			<p className={`question-answer ${id === open ? "open" : ""}`}>{answer}</p>
 		</div>
 	);
 }
--- src/Components/QuestionsContainer.jsx
+++ src/Components/QuestionsContainer.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Question from "./Question";
 import "./QuestionContainer.scss";
 
 const questions = [
 	{
+		id: 1,
 		question: "What is Frontend Mentor, and how will it help me?",
 		answer:
 			"Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
 	},
 	{
+		id: 2,
 		question: "Is Frontend Mentor free?",
 		answer:
 			"Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
 	},
 	{
+		id: 3,
 		question: "Can I use Frontend Mentor projects in my portfolio?",
 		answer:
 			"Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
 	},
 	{
+		id: 4,
 		question: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
 		answer:
 			"The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
@@ -26,16 +30,25 @@ const questions = [
 ];
 
 function QuestionsContainer() {
+	const [open, setOpen] = useState(null);
+
 	return (
 		<div className="question-container">
-			<h1>
+			<div>
 				<span>
 					<img src="/images/icon-star.svg" alt="Star icon" />
 				</span>
-				FAQs
-			</h1>
+				<h1>FAQs</h1>
+			</div>
 			{questions.map((item) => (
-				<Question question={item.question} answer={item.answer} />
+				<Question
+					question={item.question}
+					answer={item.answer}
+					id={item.id}
+					key={item.id}
+					open={open}
+					setOpen={setOpen}
+				/>
 			))}
 		</div>
 	);
